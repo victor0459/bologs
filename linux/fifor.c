62e53df9c3bc75c5604ef7727803bd09ec1302c0
immol <349084333@qq.com>
2021-05-08 09:08:55 +0800

    fifo

---
+++ linux/fifor.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+
+int main(int argc,char*argv[]){
+	//打开fifo文件
+	if(argc!=2){
+		printf("./a.out fifoname\n");
+		return -1;
+	}
+	//写
+	int fd=open(argv[1],O_RDONLY);
+
+	char buf[256];
+	int ret;
+	while(1){
+		memset(buf,0x00,sizeof(buf));
+		ret=read(fd,buf,sizeof(buf));
+		if(ret>0){
+			printf("read %s\n",buf);
+		}
+	}
+	close(fd);
+	return 0;
+}
