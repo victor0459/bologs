6f1aeb31e1bbd2ad3f23eb6bbc2959627a3f2471
immol <349084333@qq.com>
2021-05-08 18:31:30 +0800

    signal

---
+++ linux/sigpending.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<signal.h>
+
+int main(){
+	sigset_t pend,sigproc;
+	sigemptyset(&sigproc);//先清空
+	sigaddset(&sigproc,SIGINT);//阻塞2号信号
+	sigprocmask(SIG_BLOCK,&sigproc,NULL);
+	while(1){
+		sigpending(&pend);
+		int i=1;
+		for(i=1;i<32;++i){
+			if(sigismember(&pend,i)==1){
+				printf("1");
+			}else{
+				printf("0");
+			}
+
+		}
+		printf("\n");
+		sleep(1);
+	}
+	return 0;
+}
