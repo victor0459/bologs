389ab24d0df8923f381cddfb4bb1fd7391aea1a2
immol <349084333@qq.com>
2021-05-10 17:24:35 +0800

    rwlock

---
+++ linux/mutex_trylock.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<pthread.h>
+#include<string.h>
+pthread_mutex_t mutex;
+void*thr(void *arg){
+	while(1){
+		pthread_mutex_lock(&mutex);
+		printf("hello world\n");
+		sleep(10);
+		pthread_mutex_unlock(&mutex);
+	}
+	return NULL;	
+}
+int main(){
+		pthread_mutex_init(&mutex,NULL);
+		pthread_t tid;
+		pthread_create(&tid,NULL,thr,NULL);
+		while(1){
+			int ret=pthread_mutex_trylock(&mutex);
+			if(ret>0){
+				printf("ret=%d,srrmsg:%s\n",ret,strerror(ret));
+	
+			}
+			sleep(1);
+		}
+		return 0;
+}
