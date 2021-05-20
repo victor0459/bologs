77f47f84abd7e54c53ff1623afc6a4af3f6a9a49
immol <349084333@qq.com>
2021-05-20 18:59:59 +0800

    mysql_init

---
+++ mysql/01_hello.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<string.h>
+#include"mysql.h"
+
+#define _HOST_ "127.0.0.1"
+#define _USER_ "root"
+#define _PASSWD_ "root"
+#define _DBNAME_ "scott"
+
+int main(){
+	//init
+	MYSQL*mysql=mysql_init(NULL);
+	if(mysql==NULL){
+		printf("init err\n");
+		exit(1);
+	}
+	//real_connnect
+	mysql=mysql_real_connect(mysql,_HOST_,_USER_,_PASSWD_,_DBNAME_,0,NULL,0);
+	if(mysql==NULL){
+		printf("connect err\n");
+		exit(1);
+	}
+	printf("hello mysql!\n");
+	//close
+	mysql_close(mysql);
+	return 0;
+
+}
