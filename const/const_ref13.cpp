d511f54e6b4e63fa7c9229af95a70621b8b4aa0c
immol <349084333@qq.com>
2021-06-28 10:49:54 +0800

    3dmax

---
+++ const/const_ref13.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+void test01() {
+	//int  &ref = 10;  引用不合法的内存 不可以
+	const int& ref = 10;//加入const后的处理方式 int tmp=10;const int &ref=tmp;
+	int* p =(int* )&ref;
+	*p = 1000;
+	cout << "ref=" << ref << endl;
+}
+//常量引用场景 用来修饰形参
+ 
+
+
+int main() {
+	test01();
+	return 0;
+}
