3a6e5d15aa0cc6467592b5f76d00c41a8efbdd5e
immol <349084333@qq.com>
2021-09-05 17:17:34 +0800

    lars

---
+++ lars/lars_reporter/include/store_report.h
@@ -0,0 +1,14 @@
+#include "lars.pb.h"
+#include"mysql.h"
+#include "lars_reactor.h"
+class StoreReport{
+	public:
+		StoreReport();
+
+		void store(lars::ReportStatusRequest &req);
+	private:
+		MYSQL _db_conn;
+
+
+};
+void *store_main(void*);
