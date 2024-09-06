c87850c23bbe61715cdd15c8249090c04bbfed85
victor0459 <280369402@qq.com>
2024-09-06 17:30:57 +0800

    sub

--- lars/lars_dns/src/subcribe.cpp
+++ lars/lars_dns/src/subcribe.cpp
@@ -2,3 +2,12 @@
 #include "lars.pb.h"
 
 extern tcp_server*server;
+SubcribeList*SubcribeList:: instance=NULL;
+pthread_once_t SubcribeList::_once=PTHREAD_ONCE_INIT;
+SubcribeList::SubcribeList()
+{
+	pthread_mutex_init(&_book_list_lock,NULL);
+	pthread_mutex_init(&_push_list_lock,NULL);
+
+
+}
