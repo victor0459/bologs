33a5c82dbbfaa49f84ca0e583fcadedda4793ea1
victor0459 <280369402@qq.com>
2024-10-14 14:42:07 +0800

    lars api

--- lars/api/cpp/lars_api/lars_api.cpp
+++ lars/api/cpp/lars_api/lars_api.cpp
@@ -163,6 +163,87 @@ void lars_client::report(int modid, int cmdid, std::string &ip, int port, int re
     }
 }
 
+//lars 获取某modid/cmdid的全部的hosts
+int lars_client::get_route(int modid, int cmdid, route_set &route)
+{
+    //1 封装请求的protobuf消息
+    lars::GetRouteRequest req; 
+    req.set_modid(modid);
+    req.set_cmdid(cmdid);
+    
+    
+    //2 打包成lars能够识别的message
+    char write_buf[4096], read_buf[20*4096];
+    //消息头
+    msg_head head;
+    head.msglen = req.ByteSizeLong();
+    head.msgid = lars::ID_API_GetRouteRequest;
+
+    memcpy(write_buf, &head, MESSAGE_HEAD_LEN);
+    req.SerializeToArray(write_buf + MESSAGE_HEAD_LEN, head.msglen);
+
+
+    //3 发送
+    int index = (modid + cmdid) %3;
+    int ret = sendto(_sockfd[index], write_buf, head.msglen + MESSAGE_HEAD_LEN, 0, NULL, 0);
+    if (ret == -1) {
+        perror("send to");
+        return lars::RET_SYSTEM_ERROR;
+    }
+    
+    //4 阻塞等待接收数据
+    int message_len;
+    lars::GetRouteResponse rsp;
+
+    message_len = recvfrom(_sockfd[index], read_buf, sizeof(read_buf), 0, NULL, 0);
+    if (message_len == -1) {
+        perror("recvfrom ");
+        return lars::RET_SYSTEM_ERROR;
+    }
+
+    //消息头
+    memcpy(&head, read_buf, MESSAGE_HAED_LEN);
+    if (head.msgid != lars::ID_API_GetRouteResponse) {
+        fprintf(stderr, "message IDerrror\n");
+        return lars::RET_SYSTEM_ERROR;
+    }
+    
+    //消息体
+    ret = rsp.ParseFromArray(read_buf + MESSAGE_HEAD_LEN, message_len-MESSAGE_HEAD_LEN);
+    if (!ret) {
+        fprintf(stderr, "message format error\n");
+        return lars::RET_SYSTEM_ERROR;
+    }
+
+
+
+    if (rsp.modid() != modid || rsp.cmdid() != cmdid) {
+        fprintf(stderr, "message format error\n");
+        return lars::RET_SYSTEM_ERROR;
+    }
+    
+    //5 处理消息
+    for (int i = 0; i < rsp.host_size(); i++) {
+        const lars::HostInfo &host = rsp.host(i);
+        //将ip的网络字节序转换成主机字节序
+        uint32_t host_ip = ntohl(host.ip()); 
+
+        // ip 
+        struct in_addr inaddr;
+        inaddr.s_addr = host_ip;
+        std::string ip = inet_ntoa(inaddr);
+
+        // port
+        int port = host.port();
+        
+        // ip+port --> route
+        route.push_back(ip_port(ip, port));
+    }
+    
+    return lars::RET_SUCC;
+}
+
+
 //注册一个模块
 int lars_client::reg_init(int modid, int cmdid)
 {
