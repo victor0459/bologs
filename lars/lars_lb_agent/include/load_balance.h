aeb1181509a291c7757e51b917315d8c24cdb76e
victor0459 <280369402@qq.com>
2024-10-20 12:56:49 +0800

    load balance

--- lars/lars_lb_agent/include/load_balance.h
+++ lars/lars_lb_agent/include/load_balance.h
@@ -13,3 +13,38 @@ typedef std::list<host_info*> host_list;
 typedef std::list<host_info*>::iterator host_list_it;
 
 
+/*
+ * 负载均衡模块
+ * 是针对一个(modid/cmdid)下的全部的host节点做负载规则
+ * */
+class load_balance {
+public:
+    load_balance(int modid, int cmdid) {
+        _modid = modid;
+        _cmdid = cmdid;
+        _access_cnt = 0;
+        status = NEW;
+        last_update_time = time(NULL);    
+    }
+
+    bool empty() {
+        return _host_map.empty();
+    }
+
+private:
+    int _modid;
+    int _cmdid;
+
+    //host_map 当前负载均衡模块所管理的全部主机
+    host_map _host_map;
+    
+    //空闲队列
+    host_list _idle_list; 
+    
+    //过载队列
+    host_list _overload_list;
+
+    //当前modid/cmdid的请求次数
+    int _access_cnt;
+
+};
