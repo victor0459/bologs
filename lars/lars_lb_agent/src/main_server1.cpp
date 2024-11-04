6f15e103739d82d90f972e93076d8828157916c0
victor0459 <280369402@qq.com>
2024-11-04 17:54:22 +0800

    mainserver1

---
+++ lars/lars_lb_agent/src/main_server1.cpp
@@ -0,0 +1,44 @@
+#include "main_server.h"
+
+thread_queue<lars::ReportStatusRequest>* report_queue = NULL;
+thread_queue<lars::GetRouteRequest>* dns_queue = NULL;
+
+
+//每个Agent UDP server   会对应一个 Route_lb
+route_lb *r_lb[3];
+
+struct load_balance_config lb_config;
+
+int main(int argc, char **argv)
+{
+    init_lb_agent();
+
+    //1 启动udp server服务器(3个 udp server) 用来接收(业务层-开发者-api层)发来的消息
+    start_UDP_servers();
+    
+    //2 启动 lars_reporter client 线程
+    report_queue = new thread_queue<lars::ReportStatusRequest>();
+    if (report_queue == NULL) {
+        fprintf(stderr, "create report queue error\n");
+        exit(1);
+    }
+    start_report_client();
+
+    
+    //3 启动 lars_dns client 线程
+    dns_queue = new thread_queue<lars::GetRouteRequest>();
+    if (dns_queue == NULL) {
+        fprintf(stderr, "create dns queue error\n");
+        exit(1);
+    }
+    start_dns_client();
+
+    
+    
+    //4 主线程应该是阻塞状态
+    while (1) {
+        sleep(10);
+    }
+
+    return 0;
+}
