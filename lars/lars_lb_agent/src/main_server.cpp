de8f2d767cccd4cbc5a4aacb42f97c37d6e39bf5
victor0459 <280369402@qq.com>
2024-11-09 19:28:17 +0800

    mainserver

--- lars/lars_lb_agent/src/main_server.cpp
+++ lars/lars_lb_agent/src/main_server.cpp
@@ -9,10 +9,12 @@ route_lb *r_lb[3];
 
 struct load_balance_config lb_config;
 
+
 void init_lb_agent() 
 {
     //设置配置文件路径(只能执行一次)
     config_file::setPath("./conf/lb_agent.ini");
+
     lb_config.probe_num = config_file::instance()->GetNumber("loadbalance", "probe_num", 10);
     lb_config.init_succ_cnt = config_file::instance()->GetNumber("loadbalance", "init_succ_cnt", 180);
     lb_config.init_err_cnt = config_file::instance()->GetNumber("loadbalance", "init_err_cnt", 5);
@@ -20,6 +22,22 @@ void init_lb_agent()
     lb_config.succ_rate = config_file::instance()->GetFloat("loadbalance", "succ_rate", 0.95);
     lb_config.contin_succ_limit = config_file::instance()->GetNumber("loadbalance", "contin_succ_limit", 10);
     lb_config.contin_err_limit = config_file::instance()->GetNumber("loadbalance", "contin_err_limit", 10);
+    lb_config.idle_timeout = config_file::instance()->GetNumber("loadbalance", "idle_timeout", 15);
+    lb_config.overload_timeout = config_file::instance()->GetNumber("loadbalance", "overload_timeout", 15);
+    lb_config.update_timeout = config_file::instance()->GetNumber("loadbalance", "update_timeout", 15);
+
+
+    for (int i = 0; i < 3 ; i++) {
+        int id = i + 1; //route_lb 的id是从 1 开始计数的
+        r_lb[i] = new route_lb(id);
+        if (r_lb[i] == NULL) {
+            fprintf(stderr, "create route lb  id = %d error\n", id);
+            exit(1);
+        }
+    }
+
+    //TODO 加载本地ip
+    
 }
 
 int main(int argc, char **argv)
