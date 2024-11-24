ac888eacdd4dff3b6d2fcdf72a004807462f4eea
victor0459 <280369402@qq.com>
2024-11-24 18:01:37 +0800

    agentudp24

--- lars/lars_lb_agent/src/agent_udp_server1.cpp
+++ lars/lars_lb_agent/src/agent_udp_server1.cpp
@@ -8,3 +8,8 @@ void report_cb(const char *data, uint32_t len, int msgid, net_connection *conn, 
     route_lb *route_lb_p = (route_lb*)user_data;
     route_lb_p->report_host(req);
 }
+void get_host_cb(const char *data, uint32_t len, int msgid, net_connection *conn, void *user_data)
+{
+    lars::GetHostRequest req;
+    req.ParseFromArray(data, len);
+}
