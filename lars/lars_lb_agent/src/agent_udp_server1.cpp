836e1abcf01a8b411dd57486120ca71d148c6f88
victor0459 <280369402@qq.com>
2024-11-28 13:10:55 +0800

    udpserver28

--- lars/lars_lb_agent/src/agent_udp_server1.cpp
+++ lars/lars_lb_agent/src/agent_udp_server1.cpp
@@ -22,4 +22,8 @@ void get_host_cb(const char *data, uint32_t len, int msgid, net_connection *conn
     //通过 route_lb 获取一个可用host 添加到rsp中
     route_lb *route_lb_p = (route_lb*)user_data;
     route_lb_p->get_host(modid, cmdid, rsp);
+    //将 rsp 发送回给 api 
+    std::string responseString;
+    rsp.SerializeToString(&responseString);
+    conn->send_message(responseString.c_str(), responseString.size(), lars::ID_GetHostResponse);
 }
