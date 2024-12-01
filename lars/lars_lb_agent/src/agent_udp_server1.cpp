899a1975d7492736d047c55cc57778f746bf55cf
victor0459 <280369402@qq.com>
2024-12-01 15:05:40 +0800

    agentudp1

--- lars/lars_lb_agent/src/agent_udp_server1.cpp
+++ lars/lars_lb_agent/src/agent_udp_server1.cpp
@@ -38,6 +38,7 @@ void *agent_server_main(void* args)
     udp_server server(&loop, "0.0.0.0", port) ;
 
     //给udp server注册一些消息路由业务
-
+    //针对API的获取主机信息接口
+    server.add_msg_router(lars::ID_GetHostRequest, get_host_cb, r_lb[port-8888]); //8888->r_lb[0], 8889->r_lb[1], 8890->r_lb[2]
     return NULL;
 }
