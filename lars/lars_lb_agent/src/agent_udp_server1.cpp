38a4baebb832eb2379d0d807e40446b7e4dc147e
victor0459 <280369402@qq.com>
2024-12-03 16:03:45 +0800

    udpserver3

--- lars/lars_lb_agent/src/agent_udp_server1.cpp
+++ lars/lars_lb_agent/src/agent_udp_server1.cpp
@@ -42,5 +42,7 @@ void *agent_server_main(void* args)
     server.add_msg_router(lars::ID_GetHostRequest, get_host_cb, r_lb[port-8888]); //8888->r_lb[0], 8889->r_lb[1], 8890->r_lb[2]
     //针对API的上报主机调用结果接口
     server.add_msg_router(lars::ID_ReportRequest, report_cb, r_lb[port-8888]);
+    //针对API获取路由全部主机信息的接口
+    server.add_msg_router(lars::ID_API_GetRouteRequest, get_route_cb, r_lb[port-8888]);
     return NULL;
 }
