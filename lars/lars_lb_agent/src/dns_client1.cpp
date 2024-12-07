a3acbf12adb3c0ebec5d007a6202f41fdf29c5e9
victor0459 <280369402@qq.com>
2024-12-07 12:23:39 +0800

    dnsc7

---
+++ lars/lars_lb_agent/src/dns_client1.cpp
@@ -0,0 +1,18 @@
+#include "main_server.h"
+
+
+//dns service已经恢复当前modid/cmdid对应的全部的主机集合
+void deal_recv_route(const char *data, uint32_t len, int msgid, net_connection *conn, void *user_data)
+{
+    lars::GetRouteResponse rsp;
+    rsp.ParseFromArray(data, len);
+
+    int modid = rsp.modid();
+    int cmdid = rsp.cmdid();
+
+    int index = (modid+cmdid) % 3;
+
+    //将 modid/cmdid交给一个route_lb来处理，将rsp中的hostinfo集合加入到对应route_lb中 
+    r_lb[index]->update_host(modid, cmdid, rsp);
+}
+
